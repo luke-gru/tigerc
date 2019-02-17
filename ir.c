8e7c673fa1eb0f40bdbb776fb482e3eefb310c2b
Luke Gruber <luke.gru@gmail.com>
2019-02-16 20:23:59 -0500

    add ir.h and ir.c struct definitions/factory functions

---
+++ ir.c
@@ -0,0 +1,106 @@
+#include "util.h"
+#include "ir.h"
+
+static IrStmt AllocIrStmt(int kind) {
+    IrStmt p = CHECKED_MALLOC(struct sIrStmt);
+    p->kind = kind;
+    return p;
+}
+
+static IrExpr AllocIrExpr(int kind) {
+    IrExpr p = CHECKED_MALLOC(struct sIrExpr);
+    p->kind = kind;
+    return p;
+}
+
+IrStmt Ir_Seq_Stmt(List seq) {
+    IrStmt stmt = AllocIrStmt(tIrSeq);
+    stmt->as.seq = seq;
+    return stmt;
+}
+
+IrStmt Ir_Label_Stmt(TempLabel label) {
+    IrStmt stmt = AllocIrStmt(tIrLabel);
+    stmt->as.label = label;
+    return stmt;
+}
+
+IrStmt Ir_Jump_Stmt(IrExpr expr, List jumps) {
+    IrStmt stmt = AllocIrStmt(tIrJump);
+    stmt->as.jump.expr = expr;
+    stmt->as.jump.jumps = jumps;
+    return stmt;
+}
+
+IrStmt Ir_Cjump_Stmt(IrRelop op,
+                        IrExpr left,
+                        IrExpr right,
+                        TempLabel t,
+                        TempLabel f) {
+    IrStmt stmt = AllocIrStmt(tIrCjump);
+    stmt->as.cjump.op = op;
+    stmt->as.cjump.left = left;
+    stmt->as.cjump.right = right;
+    stmt->as.cjump.t = t;
+    stmt->as.cjump.f = f;
+    return stmt;
+}
+
+IrStmt Ir_Move_Stmt(IrExpr dst, IrExpr src) {
+    IrStmt stmt = AllocIrStmt(tIrMove);
+    stmt->as.move.dst = dst;
+    stmt->as.move.src = src;
+    return stmt;
+}
+
+IrStmt Ir_Expr_Stmt(IrExpr expr) {
+    IrStmt stmt = AllocIrStmt(tIrExpr);
+    stmt->as.expr = expr;
+    return stmt;
+}
+
+IrExpr Ir_Binop_Expr(IrBinop op, IrExpr left, IrExpr right) {
+    IrExpr p = AllocIrExpr(tIrBinop);
+    p->as.binop.op = op;
+    p->as.binop.left = left;
+    p->as.binop.right = right;
+    return p;
+}
+
+IrExpr Ir_Mem_Expr(IrExpr mem) {
+    IrExpr p = AllocIrExpr(tIrMem);
+    p->as.mem = mem;
+    return p;
+}
+
+IrExpr Ir_Tmp_Expr(Temp tmp) {
+    IrExpr p = AllocIrExpr(tIrTmp);
+    p->as.tmp = tmp;
+    return p;
+}
+
+IrExpr Ir_Eseq_Expr(IrStmt stmt, IrExpr expr) {
+    IrExpr p = AllocIrExpr(tIrEseq);
+    p->as.eseq.stmt = stmt;
+    p->as.eseq.expr = expr;
+    return p;
+}
+
+IrExpr Ir_Name_Expr(TempLabel name) {
+    IrExpr p = AllocIrExpr(tIrName);
+    p->as.name = name;
+    return p;
+}
+
+IrExpr Ir_Const_Expr(int const_) {
+    IrExpr p = AllocIrExpr(tIrConst);
+    p->as.const_ = const_;
+    return p;
+}
+
+IrExpr Ir_Call_Expr(IrExpr func, List args) {
+    IrExpr p = AllocIrExpr(tIrCall);
+    p->as.call.func = func;
+    p->as.call.args = args;
+    return p;
+}
---
+++ ir.h
@@ -0,0 +1,65 @@
+#ifndef tiger_ir_h
+#define tiger_ir_h
+
+#include "temp.h"
+#include "util.h"
+
+typedef struct sIrStmt *IrStmt;
+typedef struct sIrExpr *IrExpr;
+
+typedef enum {
+    IR_PLUS, IR_MINUS, IR_MUL, IR_DIV,
+    IR_AND, IR_OR, IR_XOR,
+    IR_LSHIFT, IR_RSHIFT, IR_ARSHIFT,
+} IrBinop;
+
+typedef enum {
+    IR_EQ, IR_NE, IR_LT, IR_LE, IR_GT, IR_GE,
+    IR_ULT, IR_ULE, IR_UGT, IR_UGE,
+} IrRelop;
+
+struct sIrStmt {
+    enum { tIrSeq, tIrLabel, tIrJump, tIrCjump, tIrMove, tIrExpr } kind;
+    union {
+        List seq;
+        TempLabel label;
+        struct { IrExpr expr; List jumps; } jump;
+        struct { IrRelop op; IrExpr left, right; TempLabel t, f; } cjump;
+        struct { IrExpr dst, src; } move;
+        IrExpr expr;
+    } as;
+};
+
+IrStmt Ir_Seq_Stmt(List seq);
+IrStmt Ir_Label_Stmt(TempLabel label);
+IrStmt Ir_Jump_Stmt(IrExpr expr, List jumps);
+IrStmt Ir_Cjump_Stmt(IrRelop op,
+                        IrExpr left,
+                        IrExpr right,
+                        TempLabel t,
+                        TempLabel f);
+IrStmt Ir_Move_Stmt(IrExpr dst, IrExpr src);
+IrStmt Ir_Expr_Stmt(IrExpr expr);
+
+struct sIrExpr {
+    enum { tIrBinop, tIrMem, tIrTmp, tIrEseq, tIrName, tIrConst, tIrCall } kind;
+    union {
+        struct { IrBinop op; IrExpr left, right; } binop;
+        IrExpr mem;
+        Temp tmp;
+        struct { IrStmt stmt; IrExpr expr; } eseq;
+        TempLabel name;
+        int const_;
+        struct { IrExpr func; List args; } call;
+    } as;
+};
+
+IrExpr Ir_Binop_Expr(IrBinop op, IrExpr left, IrExpr right);
+IrExpr Ir_Mem_Expr(IrExpr mem);
+IrExpr Ir_Tmp_Expr(Temp tmp);
+IrExpr Ir_Eseq_Expr(IrStmt stmt, IrExpr expr);
+IrExpr Ir_Name_Expr(TempLabel name);
+IrExpr Ir_Const_Expr(int const_);
+IrExpr Ir_Call_Expr(IrExpr func, List args);
+
+#endif
